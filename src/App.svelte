7b612a60a0ae64ebb0c69379964a68dd08168718
Aravind Vasudevan <aravind_vasudevan@outlook.com>
2023-03-26 13:25:09 -0700

    Add initial implementation
    This change adds initial working POC for the regex matcher.

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,30 @@
 <script lang="ts">
-let name = "Aravind";
+  let expression = "";
+  let example = "";
+  let examples = 1;
+  let valid = false;
 </script>
 
 <main>
-Hello {name}!
+<input
+  type="text"
+  bind:value={expression}
+  class="expression"
+  placeholder="Enter regex here..." />
+
+  <br />
+
+  {#each Array(examples) as _}
+    <input
+      bind:value={example}
+      placeholder="Enter example here" />
+      {#if example && example.match(expression)}👍{/if}
+  {/each}
 </main>
 
 <style>
+  .expression {
+    display: block;
+    width: 80%;
+  }
 </style>
