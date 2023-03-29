061a543522199305a3a601724d3e973d8d864513
Aravind Vasudevan <aravind_vasudevan@outlook.com>
2023-03-29 02:33:14 -0700

    Add highlighting feature
    This change adds feature to highlight text within the textarea.

--- src/App.svelte
+++ src/App.svelte
@@ -1,19 +1,53 @@
 <script lang="ts">
+  // Data-binded html elements.
+  let backdropDiv;
+  let highlightDiv;
+  let exampleTextArea;
+
+  // Data-binded values.
   let expression = "";
   let example = "";
 
-  $: if (example && expression && example.match(expression)) {
-    console.log("match");
+  // Add highlights within the text area.
+  function highlight() {
+    let highlight = example
+      .replace(/\n$/g, "\n\n")
+      .replace(/[A-Z].*?\b/g, "<mark>$&</mark>");
+
+    highlightDiv.innerHTML = highlight;
+  }
+
+  // Handles scrolling highlights within the text area.
+  function scroll() {
+    backdropDiv.scrollTop = exampleTextArea.scrollTop;
+    backdropDiv.scrollLeft = exampleTextArea.scrollLeft;
   }
 </script>
 
 <main>
-<div id="expression">
-  <label for="expression">Expression</label>
-  <input type="text" bind:value={expression} placeholder="Enter regex here..." />
-</div>
-<div id="divider"></div>
-<textarea name="example" id="example" bind:value={example}></textarea>
+  <div id="expression">
+    <label for="expression">Expression</label>
+    <input
+      type="text"
+      bind:value={expression}
+      placeholder="Enter regex here..."
+    />
+  </div>
+  <div id="divider" />
+  <div id="example">
+    <div class="backdrop" bind:this={backdropDiv}>
+      <div class="highlights" bind:this={highlightDiv}>
+        <!-- This section would be populated with highlighted text. -->
+      </div>
+    </div>
+    <textarea
+      name="example"
+      bind:this={exampleTextArea}
+      bind:value={example}
+      on:input={highlight}
+      on:scroll={scroll}
+    />
+  </div>
 </main>
 
 <style>
@@ -21,39 +55,33 @@
     /* Center the frame. */
     height: 80vh;
     width: 80vw;
-    box-sizing: border-box;
 
     margin: auto;
     display: flex;
     flex-direction: column;
 
-    border: 0.1em solid #DD4B1A;
+    border: 0.1em solid #dd4b1a;
   }
 
   #expression > label {
     /* label is set to be a full length block. */
     display: block;
-    box-sizing: border-box;
     width: 100%;
     flex: 1;
 
     padding: 0.25em;
     padding-left: 0.5em;
 
-    font-family: 'Space Mono', monospace;
     font-weight: bold;
-    font-size: 1em;
-    color: #FFEEDB;
-    
-    background-color: #DD4B1A;
+    color: #ffeedb;
+
+    background-color: #dd4b1a;
   }
 
   #expression > input {
     width: 100%;
-    box-sizing: border-box;
     flex: 1;
 
-    font-family: 'Space Mono', monospace;
     font-weight: bold;
     font-size: 1em;
 
@@ -61,7 +89,7 @@
     padding-right: 0.5em;
     border: 0;
 
-    background-color: #FFEEDB;
+    background-color: #ffeedb;
   }
 
   #divider {
@@ -69,21 +97,63 @@
     width: 100%;
     flex: 0.25;
 
-    background-color: #DD4B1A;
+    background-color: #dd4b1a;
   }
 
   #example {
     width: 100%;
-    box-sizing: border-box;
     flex: 10;
 
-    background-color: #FFEEDB;
+    position: relative;
+  }
 
-    font-family: 'Space Mono', monospace;
+  #example > textarea {
+    display: block;
+    width: 100%;
+    height: 100%;
+
+    border-radius: 0;
+    padding-left: 0.5em;
+    padding-right: 0.5em;
+
+    position: absolute;
+    z-index: 2;
+    resize: none;
+    overflow: auto;
+
+    background-color: transparent;
   }
 
   #expression > input:focus,
-  #example:focus {
+  #example > textarea:focus {
     outline: none;
+  }
+
+  #example > .backdrop {
+    width: 100%;
+    height: 100%;
+
+    position: absolute;
+    padding-left: 0.5em;
+    padding-right: 0.5em;
+
+    overflow: auto;
+    pointer-events: none;
+
+    background-color: #ffeedb;
+  }
+
+  #example > .backdrop > .highlights {
+    white-space: pre-wrap;
+    word-wrap: break-word;
+
+    color: transparent;
+  }
+
+  :global(mark) {
+    color: transparent;
+
+    border-radius: 3px;
+    background-color: #73c2be;
   }
 </style>
